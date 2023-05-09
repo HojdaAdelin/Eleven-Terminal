ba4f152c04d08c15a044feec055357c86c30b78f
HojdaAdelin <adelinionut297@gmail.com>
2023-05-09 21:45:02 +0300

    fix 1

--- components/info/content.cpp
+++ components/info/content.cpp
@@ -9,14 +9,14 @@ void areas() {
     
     cout << "Eleven Terminal - Geometric areas" << endl;
     cout << "\n1. Triangle: b * h / 2" << endl;
-    cout << "2. Equilateral triangle: l^2sqrt(3) \ 4" << endl;
+    cout << "2. Equilateral triangle: l^2sqrt(3) / 4" << endl;
     cout << "3. Square: l^2" << endl;
-    cout << "4. Diamond: d1 * d2 \ 2" << endl;
-    cout << "5. Trapeze: (B + b) * h \ 2" << endl;
+    cout << "4. Diamond: d1 * d2 / 2" << endl;
+    cout << "5. Trapeze: (B + b) * h / 2" << endl;
     cout << "6. Right angle: L * l" << endl;
-    cout << "7. Right angle triangle: c1 * c2 \ 2" << endl;
+    cout << "7. Right angle triangle: c1 * c2 / 2" << endl;
 
-    cout << "[9] Exit" << endl;
+    cout << "\n[9] Exit" << endl;
     cin >> aws;
 
     if (aws == 9) {
@@ -40,7 +40,7 @@ void algebra() {
     cout << "6. Trapeze Area: ((b+B) * h) / 2" << endl;
     cout << "7. Circle Area: r * r * 3.14" << endl;
 
-    cout << "[9] Exit" << endl;
+    cout << "\n[9] Exit" << endl;
     cin >> aws;
     if (aws == 9) {
         system("cls");
